e65134ae7e16a3e6014257cbbc89061a85eb3b02
Chilo Nwokolo <despeakableme@gmail.com>
2020-06-22 20:52:58 +0100

    modified gitignore

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-burger-tutorial-266014.firebaseio.com',
+    baseURL: 'https://react-burger-tutorial-266014.firebaseio.com',  
 })
-export default instance;
+export default instance
